75baaf6b836b7de5f48e1c30595b846477ff2724
JeevanAchar <jeevanachar83@gmail.com>
2024-07-23 18:58:29 +0530

    implemented search functionality

--- src/components/task/TaskContainer.jsx
+++ src/components/task/TaskContainer.jsx
@@ -5,6 +5,36 @@ import { useTask } from '../../context/TaskContext';
 
 function TaskContainer() {
     const { tasks, selectedTask, setSelectedTask, isFormOpened, setIsFormOpened, handleAddTask, handleEditTask } = useTask();
+    const [searchTask, setSearchTask] = React.useState("");
+    const [sortTask, setSortTask] = React.useState("recent");
+    const [filteredTasks, setFilteredTasks] = React.useState([]);
+
+    const fetchFilteredTask = () => {
+        let filtered = tasks;
+
+        if (searchTask) {
+            filtered = filtered.filter((task) => {
+                return task.title.toLowerCase().includes(searchTask.toLowerCase()) ||
+                    task.description.toLowerCase().includes(searchTask.toLowerCase())
+            });
+        }
+
+        if (sortTask === "recent") {
+            filtered = filtered.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        } else {
+            filtered = filtered.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        }
+
+        setFilteredTasks(filtered);
+    }
+
+    React.useEffect(() => {
+        try {
+            fetchFilteredTask()
+        } catch (err) {
+            console.error(err);
+        }
+    }, [tasks, searchTask, sortTask]);
 
     return (
         <div className="p-4">
@@ -12,22 +42,42 @@ function TaskContainer() {
             <div className="bg-white shadow-md md:p-4 p-2 rounded flex justify-between items-center mb-6">
                 <div className="flex items-center gap-2">
                     <label htmlFor="search" className='text-sm md:text-base'>Search:</label>
-                    <input type="text" id="search" name="search" placeholder="Search..." className="max-w-28 md:max-w-xs px-4 py-1 border border-gray-500 outline-gray-500 rounded" />
+                    <input
+                        type="text"
+                        id="search"
+                        name="search"
+                        placeholder="Search..."
+                        className="max-w-28 md:max-w-xs px-4 py-1 border border-gray-500 outline-gray-500 rounded"
+                        value={searchTask}
+                        onChange={(e) => setSearchTask(e.target.value)}
+                    />
                 </div>
                 <div className="flex items-center md:gap-4">
                     <label htmlFor="sort" className='text-sm md:text-base'>Sort By:</label>
-                    <select name="sort" id="sort" className="px-3 py-1 border border-gray-500 outline-gray-500 rounded">
+                    <select
+                        name="sort"
+                        id="sort"
+                        className="px-3 py-1 border border-gray-500 outline-gray-500 rounded"
+                        value={sortTask}
+                        onChange={(e) => setSortTask(e.target.value)}
+                    >
                         <option value="recent">Recent</option>
                         <option value="oldest">Oldest</option>
                     </select>
                 </div>
             </div>
             <div className="md:flex w-full">
-                <TaskColumn title="todo" />
-                <TaskColumn title="In progress" />
-                <TaskColumn title="done" />
+                <TaskColumn title="todo" tasks={filteredTasks.filter(task => task.status === "todo")} />
+                <TaskColumn title="In progress" tasks={filteredTasks.filter(task => task.status === "in-progress")} />
+                <TaskColumn title="done" tasks={filteredTasks.filter(task => task.status === "done")} />
             </div>
-            {isFormOpened && <TaskForm onSave={selectedTask ? handleEditTask : handleAddTask} task={selectedTask} onClose={() => { setIsFormOpened(false); setSelectedTask(null); }} />}
+            {isFormOpened && (
+                <TaskForm
+                    onSave={selectedTask ? handleEditTask : handleAddTask}
+                    task={selectedTask}
+                    onClose={() => { setIsFormOpened(false); setSelectedTask(null); }}
+                />
+            )}
         </div>
     );
 }
