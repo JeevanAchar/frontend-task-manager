b14af84909f7a8b613ac50c1af8d71e286806a79
JeevanAchar <jeevanachar83@gmail.com>
2024-07-23 10:14:43 +0530

    added login functionality

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,34 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "http://localhost:5000",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
 });
+
+axiosInstance.interceptors.request.use((request) => {
+    request.headers["Authorization"] = window.localStorage.getItem("token");
+    return request;
+})
+
+axiosInstance.interceptors.response.use((response) => {
+    const { status } = response;
+
+    if (status < 399) {
+        return response;
+    }
+
+    if (status === 401) {
+        window.location.href = "/login";
+    }
+
+    if (status >= 500) {
+        alert("Internal Server Error");
+    }
+
+    return response;
+})
 
 export default axiosInstance;
