0b87c97cfd089f25dfb532572484638efd6689ba
JeevanAchar <jeevanachar83@gmail.com>
2024-07-23 17:29:36 +0530

    fixed input error

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -52,7 +52,7 @@ function Signup() {
             <div className="w-full max-w-md">
                 <h2 className="text-blue-600 text-2xl font-bold mb-4">Signup</h2>
                 <div className="border-2 border-blue-600 p-4 rounded-lg">
-                    <form action="" autoComplete="off" onSubmit={formik.handleSubmit}>
+                    <form onSubmit={formik.handleSubmit}>
                         <div className="my-3">
                             <input type="text" id="firstName" name="firstName" placeholder="First Name" className="w-full outline-gray-400 border border-gray-300 px-3 py-2 rounded"
                                 onChange={formik.handleChange} value={formik.values.firstName} onBlur={formik.handleBlur} />
@@ -81,7 +81,7 @@ function Signup() {
                         </div>
                         <div className="mb-4">
                             <input type="password" id="password" name="password" placeholder="Password" className="w-full outline-gray-400 border border-gray-300 px-3 py-2 rounded"
-                                onChange={formik.handleChange} value={formik.values.password} onBlur={formik.handleBlur} />
+                               autoComplete="on" onChange={formik.handleChange} value={formik.values.password} onBlur={formik.handleBlur} />
                             <div>
                                 {
                                     formik.touched.password && formik.errors.password ?
@@ -92,7 +92,7 @@ function Signup() {
                         </div>
                         <div className="mb-4">
                             <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" className="w-full outline-gray-400 border border-gray-300 px-3 py-2 rounded"
-                                onChange={formik.handleChange} value={formik.values.confirmPassword} onBlur={formik.handleBlur} />
+                               autoComplete="on" onChange={formik.handleChange} value={formik.values.confirmPassword} onBlur={formik.handleBlur} />
                             <div>
                                 {
                                     formik.touched.confirmPassword && formik.errors.confirmPassword ?
